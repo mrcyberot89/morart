ed10f150d200cbfb55a9dbdc56ffc604e7e6135e
Alibyas <cybrtnox@gmail.com>
2026-03-03 01:44:10 +0700

    deploy

--- morart_backend/routes/api.php
+++ morart_backend/routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PhotoController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
