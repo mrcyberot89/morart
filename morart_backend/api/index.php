8c62f172d1de53fe8ec539abb429f1101f90e2dd
Alibyas <cybrtnox@gmail.com>
2026-03-03 05:58:45 +0700

    deploy

--- morart_backend/api/index.php
+++ morart_backend/api/index.php
@@ -1,2 +1,6 @@
 <?php
-phpinfo();
+
+use function PHPUnit\Framework\directoryExists;
+directoryExists(__DIR__.'/../vendor/autoload.php') || die("❌ Vendor autoload tidak ditemukan. Jalankan 'composer install' terlebih dahulu.");
+directoryExists(__DIR__.'/../bootstrap/app.php') || die("❌ File bootstrap/app.php tidak ditemukan. Pastikan struktur Laravel benar.");
+directoryExists(__DIR__.'/../routes/api.php') || die("❌ File routes/api.php tidak ditemukan. Pastikan struktur Laravel benar.");
