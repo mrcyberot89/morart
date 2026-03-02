e77c2d737fcf25ddfd714defd893814728666ea5
Alibyas <cybrtnox@gmail.com>
2026-03-03 06:07:51 +0700

    deploy

--- morart_backend/api/index.php
+++ morart_backend/api/index.php
@@ -1,7 +1,4 @@
 <?php
 
-use function PHPUnit\Framework\directoryExists;
+require __DIR__.'/../public/index.php';
 
-require __DIR__.'/../vendor/autoload.php';
-define('LARAVEL_START', microtime(true));
-require __DIR__.'/../bootstrap/app.php';
